fa04e92cee99875efb888417a3f736a477c2030d
kezia secilia nani <kezianani933@gmail.com>
2025-10-28 15:33:45 -1200

    first commit

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<?php
+// panggil koneksi database
+require_once __DIR__ . '/../config/database.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Universitas - Koneksi Database</title>
+</head>
+<body>
+  <h2>Koneksi ke Database Universitas Berhasil!</h2>
+  <p>Selamat, proyek PHP kamu sudah terhubung dengan database menggunakan PDO.</p>
+</body>
+</html>
